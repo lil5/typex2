441d1bcff082b381fb110654f42672386903d62d
Lucian I. Last <li@last.nl>
2020-10-18 21:50:04 +0200

    Add cli

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lil5/typex2
+
+go 1.15
+
+require (
+	github.com/urfave/cli v1.22.4
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "typex2",
+		Usage: "Convert go structs to typescript interfaces",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Hello friend!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
