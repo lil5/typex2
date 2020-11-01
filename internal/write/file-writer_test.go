0d7c0d4311220cc0bc19056ff32787b2a29a5c6d
Lucian I. Last <li@last.nl>
2020-11-01 20:28:10 +0100

    Add file writer test

---
+++ internal/write/file-writer_test.go
@@ -0,0 +1,43 @@
+package write
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestFileWriter(t *testing.T) {
+	testString := "Lorem Ipsum"
+	path := "../../storage"
+	fname := "test.ts"
+	err := FileWriter(path, fname, &testString)
+
+	if err != nil {
+		msg := fmt.Sprintf("Could not write to file path. %v", err)
+		fmt.Println(msg)
+		t.Errorf(msg)
+	}
+
+	fullpath := path + "/" + fname
+	d, err := ioutil.ReadFile(fullpath)
+
+	if err != nil {
+		msg := "Could not read test file."
+		fmt.Println(msg)
+		t.Errorf(msg)
+	}
+
+	contents := string(d)
+	if contents != testString {
+		fmt.Printf("Contents is not equal to testString:\n%s\n---\n%s\n", contents, testString)
+		t.Fail()
+	}
+
+	err = os.Remove(fullpath)
+	if err != nil {
+		msg := "Could not remove test file."
+		fmt.Println(msg)
+		t.Errorf(msg)
+	}
+}
